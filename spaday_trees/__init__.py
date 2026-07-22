1add466bec701fa38c044560d35e1f7ea110e15c
Tim Paine <3105306+timkpaine@users.noreply.github.com>
2026-07-22 19:02:04 -0400

    Add Trees component integration
    Signed-off-by: Tim Paine <3105306+timkpaine@users.noreply.github.com>

--- spaday_trees/__init__.py
+++ spaday_trees/__init__.py
@@ -1,1 +1,17 @@
+from pathlib import Path
+
+from spaday import ComponentPackage
+
+from .components import SpadayTree
+
 __version__ = "0.1.0"
+
+package = ComponentPackage(
+    name="trees",
+    assets_dir=Path(__file__).parent / "extension",
+    assets=(("js", "cdn/index.js"),),
+)
+
+Tree = SpadayTree
+
+__all__ = ["SpadayTree", "Tree", "package"]

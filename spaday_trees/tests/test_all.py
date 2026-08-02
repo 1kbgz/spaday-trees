4a398549a6ba5b0f49f12a2bd4b27ed4f2957172
Tim Paine <3105306+timkpaine@users.noreply.github.com>
2026-08-02 17:04:27 -0400

    Update test_all.py

--- spaday_trees/tests/test_all.py
+++ spaday_trees/tests/test_all.py
@@ -1,9 +1,5 @@
-<<<<<<< before updating
 import ast
 from pathlib import Path
-=======
-from spaday_trees import *
->>>>>>> after updating
 
 from spaday import generate
 from spaday.bootstrap import bootstrap
